bb0a60603c117df1f8190cbc02e614c61c2b3237
datajoely <joel.schwarzmann@quantumblack.com>
2021-11-08 13:33:46 +0000

    Update logic to be less dynamic

--- src/modular_spaceflights/pipelines/data_science/nodes.py
+++ src/modular_spaceflights/pipelines/data_science/nodes.py
@@ -18,16 +18,34 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
     Returns:
         Split data.
     """
-    X = data[parameters["features"]]
-    y = data["price"]
+    feature_columns_pattern_regex = "|".join(parameters["features_pattern"])
+
+    feature_columns_explicit = set(parameters["features_explicit"])
+    feature_columns_pattern = set(
+        data.filter(regex=feature_columns_pattern_regex).columns
+    )
+
+    columns_in_scope = feature_columns_explicit | feature_columns_pattern
+    target_variable = parameters["target"]
+    test_size = parameters["test_size"]
+    random_state = parameters["random_state"]
+
+    logger = logging.getLogger(__name__)
+    logger.info(
+        f"Splitting data for the following independent variables {columns_in_scope} against "
+        f"the target of '{target_variable}' with a test sized of {test_size} and a random state of '{random_state}'"
+    )
+
+    X = data[columns_in_scope]
+    y = data[target_variable]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=parameters["test_size"], random_state=parameters["random_state"]
+        X, y, test_size=test_size, random_state=random_state
     )
     return X_train, X_test, y_train, y_test
 
 
 def train_model(
-    X_train: pd.DataFrame, y_train: pd.Series, model_options: Dict[str, Any]
+    X_train: pd.DataFrame, y_train: pd.Series, model_options: Dict[str,Any]
 ) -> Union[LinearRegression, RandomForestRegressor]:
     """Trains the linear regression model.
 
@@ -38,15 +56,18 @@ def train_model(
     Returns:
         Trained model.
     """
-    module_name = model_options.get("module")
-    class_name = model_options.get("class")
 
-    try:
-        regressor_class = getattr(importlib.import_module(module_name), class_name)
-    except (ModuleNotFoundError, AttributeError):
-        raise ImportError(
-            f"Cannot import '{module_name}.{class_name}', please check "
-            "spelling and installed dependencies."
+    model_type = model_options.get('class')
+
+    acceptable_model_types = ['LinearRegression', 'RandomForestRegressor']
+    if model_type == acceptable_model_types[0]:
+        regressor_class = LinearRegression
+    elif model_type == acceptable_model_types[1]:
+        regressor_class = RandomForestRegressor
+    else:
+        raise ValueError(
+            f"Please provide one of {acceptable_model_types} "
+            "as acceptable arguments"
         )
 
     regressor_instance = regressor_class(**model_options.get("kwargs"))
@@ -74,5 +95,5 @@ def evaluate_model(
     logger = logging.getLogger(__name__)
     logger.info(
         f"Model has a coefficient R^2 of {score:.3f} on test data using a"
-        " regressor of type '{type(regressor)}'"
+        f" regressor of type '{type(regressor)}'"
     )
